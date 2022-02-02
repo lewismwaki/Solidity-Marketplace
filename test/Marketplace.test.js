5eeb403b74273ae2cb12cff5fd166b00e2c861c4
lewymwaki <mwakicodes@gmail.com>
2022-02-03 02:28:00 +0300

    init; create; deploy; test

---
+++ test/Marketplace.test.js
@@ -0,0 +1,26 @@
+const Marketplace = artifacts.require("../contracts/Marketplace.sol");
+
+contract("Marketplace", (accounts) => {
+  let marketplace;
+
+  before(async () => {
+    marketplace = await Marketplace.deployed();
+  });
+
+  describe("deployment", () => {
+      
+    it("deploys successfully", async () => {
+      const address = await marketplace.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, "");
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
+
+    it("has a name", async () => {
+      const name = await marketplace.name();
+      assert.equal(name, "Marketplace");
+    });
+    
+  });
+});
